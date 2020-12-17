9b081684e47972c52f796957993de29c76fd517b
gaowei <1548809662@qq.com>
2020-12-17 18:33:07 +0800

    123

---
+++ app/src/main/java/com/example/myfiind/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfiind.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myfiind.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
