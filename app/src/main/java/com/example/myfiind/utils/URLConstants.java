c8db3329e07057860b95cd8f6edf243b513f3637
gaowei <1548809662@qq.com>
2020-12-17 18:51:08 +0800

    123

---
+++ app/src/main/java/com/example/myfiind/utils/URLConstants.java
@@ -0,0 +1,8 @@
+package com.example.myfiind.utils;
+
+public class URLConstants {
+
+    public static String BASE_URL = "http://cdwan.cn:7000/";
+
+    public static String URL_ADD =BASE_URL+"";
+}
