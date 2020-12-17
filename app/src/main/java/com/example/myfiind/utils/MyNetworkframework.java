2bbec2528f25e7535ff4176e7a12d8c6d1307df2
gaowei <1548809662@qq.com>
2020-12-17 19:04:32 +0800

    123

--- app/src/main/java/com/example/myfiind/utils/MyNetworkframework.java
+++ app/src/main/java/com/example/myfiind/utils/MyNetworkframework.java
@@ -1,11 +1,33 @@
 package com.example.myfiind.utils;
 
-public class MyNetworkframework {
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.HashMap;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MyNetworkframework implements IWorkInterface {
 
     private static MyNetworkframework myNetworkframework;
+    private final ApiSeriver apiSeriver;
 
     private MyNetworkframework() {
-
+        OkHttpClient build = new OkHttpClient().newBuilder().build();
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(URLConstants.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiSeriver = retrofit.create(ApiSeriver.class);
     }
 
     public static MyNetworkframework getmyNetworkframework() {
@@ -17,5 +39,121 @@ public class MyNetworkframework {
             }
         }
         return myNetworkframework;
+    }
+
+    @Override
+    public <T> void get(String url, ICallBanck<T> callBanck) {
+        apiSeriver.get(url)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            Type[] genericInterfaces = callBanck.getClass().getGenericInterfaces();
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type type = actualTypeArguments[0];
+                            Gson gson = new Gson();
+                            T json = gson.fromJson(string, type);
+                            callBanck.getSuccess(json);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+    }
+
+    @Override
+    public <T> void post(String url, ICallBanck<T> callBanck) {
+
+        apiSeriver.post(url)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            Type[] genericInterfaces = callBanck.getClass().getGenericInterfaces();
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type type = actualTypeArguments[0];
+                            Gson gson = new Gson();
+                            T json = gson.fromJson(string, type);
+                            callBanck.getSuccess(json);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
+    }
+
+    @Override
+    public <T> void post(String url, HashMap<String, String> map, ICallBanck<T> callBanck) {
+        apiSeriver.post(url,map)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBody>() {
+                    @Override
+                    public void onSubscribe(@NonNull Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(@NonNull ResponseBody responseBody) {
+                        try {
+                            String string = responseBody.string();
+                            Type[] genericInterfaces = callBanck.getClass().getGenericInterfaces();
+                            Type[] actualTypeArguments = ((ParameterizedType) genericInterfaces[0]).getActualTypeArguments();
+                            Type type = actualTypeArguments[0];
+                            Gson gson = new Gson();
+                            T json = gson.fromJson(string, type);
+                            callBanck.getSuccess(json);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
     }
 }
